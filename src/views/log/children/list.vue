63131b1fb66220cdd3c70d6d80030d08dcc44f61
chenxueshan <1067974739@qq.com>
2020-06-19 17:28:55 +0800

    --story=1000001 --user=[chenxueshan] 日志管理添加

---
+++ src/views/log/children/list.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <el-table :data="data" border v-loading="loading">
+      <el-table-column label="通信的topic" prop="topic"></el-table-column>
+      <el-table-column label="产品KEY" prop="productKey"></el-table-column>
+      <el-table-column label="产品名称" prop="productName"></el-table-column>
+      <el-table-column label="设备名称" prop="deviceName"></el-table-column>
+      <el-table-column label="消息id" prop="messageId"></el-table-column>
+      <el-table-column label="业务编码" prop="bizCode"></el-table-column>
+      <el-table-column label="操作编码" prop="operation"></el-table-column>
+      <el-table-column label="创建时间" prop="time"></el-table-column>
+      <el-table-column label="消息" prop="message"></el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'logList',
+  props: {
+    data:[Array],
+    loading:{
+      type:[Boolean],
+      default:false
+    }
+  }
+};
+</script>

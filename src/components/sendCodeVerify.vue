05209357ae9bfc354aee3c106a62778f67bdd466
mawj0103 <mawj0103@163.com>
2020-07-14 14:21:21 +0800

    --story=100001--user=[mawenjuan]--获取图片验证限制获取

--- src/components/sendCodeVerify.vue
+++ src/components/sendCodeVerify.vue
@@ -2,7 +2,12 @@
   <el-dialog id="sedCodeVerify" title="发送验证" :visible.sync="dialogVisible" width="400px" @close="close">
     <div v-loading="loading" class="df">
       <el-input v-model="formData.code" placeholder="输入验证码" class="w200 mr20"></el-input>
-      <img :src="pic">
+      <div class="icon-con">
+        <el-image :src="pic" class="img"></el-image>
+        <div class="uploading" @click="getImg">
+          <i class="el-icon-refresh-left b f20"></i>
+        </div>
+      </div>
     </div>
     <span slot="footer" class="dialog-footer">
       <el-button @click="handleCancel">取 消</el-button>
@@ -22,7 +27,8 @@ export default {
         code: '',
         uuid: ''
       },
-      pic: ''
+      pic: '',
+      seconds: 0
     }
   },
   mounted () {
@@ -30,14 +36,29 @@ export default {
   },
   methods: {
     getImg () {
+      if (this.seconds > 0 && this.pic) {
+        this.$message.warning('15秒内禁止重复获取')
+        return
+      } else {
+        this.seconds = 16
+      }
       this.loading = true
       getCodeImg().then(res => {
         if (res.code === 200) {
           this.formData.uuid = res.data.uuid
           this.pic = res.data.pic
+          this.timer()
         }
         this.loading = false
       })
+    },
+    timer () {
+      if (this.seconds > 1) {
+        this.seconds--
+        setTimeout(this.timer, 1000)
+      } else {
+        this.seconds = 0
+      }
     },
     handleCancel () {
       this.close()
@@ -58,8 +79,24 @@ export default {
 
 <style lang="scss" scoped>
 #sedCodeVerify {
-  img {
-    height: 32px;
+  .icon-con {
+    .img {
+      height: 32px;
+    }
+    position: relative;
+    .uploading {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      text-align: center;
+      line-height: 32px;
+      background-color: rgba($color: #fff, $alpha: 0.7);
+      display: none;
+    }
+  }
+  .icon-con:hover .uploading {
+    display: block;
   }
 }
 </style>

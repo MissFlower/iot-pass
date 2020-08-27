bc709dbeff4c381f5d0a71a0b4aa26b594acb018
mawj0103 <mawj0103@163.com>
2020-08-27 14:48:50 +0800

    --story=1000001--user=[mawenjuan]---登录改版

---
+++ src/views/login/index1.vue
@@ -0,0 +1,101 @@
+<!--
+  文件作者：mawenjuan
+  创建日期：2020.6.16
+  文件说明：登录页面
+ -->
+<template>
+  <div id="login">
+    <div class="login-con1 df">
+      <div class="form-con">
+        <div class="f24 b">登录</div>
+        <el-input v-model="account" size="larger" placeholder="请输入账号名" prefix-icon="el-icon-user"></el-input>
+        <el-input v-model="password" size="larger" placeholder="请输入密码" prefix-icon="el-icon-lock" show-password />
+        <div class="wp100">
+          <el-button type="success" size="larger" class="wp100" @click="handleSubmit">登录</el-button>
+          <div class="df f12 c6 jc_sb mt10">
+            <div class="hand" @click="handleFind">忘记密码</div>
+            <div class="hand" @click="handleRedister">注册</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { login } from '@/api'
+export default {
+  data() {
+    return {
+      account: '',
+      phone: '',
+      password: ''
+    }
+  },
+  mounted() {
+    // if (this.$cookie.getValue('access_token')) {
+    //   this.$router.push('/index')
+    // }
+  },
+  methods: {
+    handleSubmit() {
+      if (
+        this.$fun.trim(this.account) === '' ||
+        this.$fun.trim(this.password) === ''
+      ) {
+        this.$message.warning('请输入账号和密码')
+        return
+      }
+      this.$store.dispatch('setLoading', true)
+      login({
+        account: this.account,
+        password: this.password
+      })
+        .then(res => {
+          if (res.code === 200) {
+            if (res.data) {
+              this.$cookie.setValue('access_token', res.data.tk)
+              this.$cookie.setValue('userName', this.account)
+              this.$store.dispatch('setUserInfo', {
+                account: this.account
+              })
+              this.$store.dispatch('setLoginStatus', true)
+              if (!res.data.email) {
+                this.$router.push('/add-email-tips')
+              } else {
+                if (this.$route.query.redirect) {
+                  this.$router.push(this.$route.query.redirect)
+                } else {
+                  this.$router.push('/index')
+                }
+              }
+            }
+          } else {
+            this.$message.warning(res.message)
+          }
+          this.$store.dispatch('setLoading', false)
+        })
+        .catch(() => {
+          this.$store.dispatch('setLoading', false)
+        })
+    },
+    handleRedister() {
+      this.$router.push('register')
+    },
+    handleFind() {
+      this.$router.push('password-find')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-image: url('../../assets/imgs/bg1.png');
+  background-size: 100% auto;
+}
+</style>
---
+++ src/views/login/register1.vue
@@ -0,0 +1,221 @@
+<!--
+  文件作者：mawenjuan
+  创建日期：2020.8.26
+  文件说明：新账号密码重置
+ -->
+<template>
+  <div id="register" v-loading="loading">
+    <div class="login-con1 df">
+      <div class="form-con">
+        <div class="f24 b">注册</div>
+        <el-form ref="form" :model="formData" label-width="0" :rules="rules" class="wp100">
+          <el-form-item prop="account">
+            <el-input v-model="formData.account" size="larger" placeholder="请输入账号名"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              v-model="formData.password"
+              size="larger"
+              placeholder="密码必须由8到14个字符包括大小写字母、数字组成"
+              show-password
+            />
+          </el-form-item>
+          <el-form-item prop="phone">
+            <el-input v-model="formData.phone" size="larger" placeholder="请输入手机号" />
+          </el-form-item>
+          <el-form-item prop="code">
+            <div class="pr wp100 df ai_c">
+              <el-input v-model="code" size="larger" placeholder="请输入验证码" class="flex1" />
+              <el-button class="ml10" size="larger" @click="handleSendCode">{{ msg }}</el-button>
+            </div>
+          </el-form-item>
+        </el-form>
+        <div class="wp100">
+          <el-button type="success" size="larger" class="wp100" @click="handleSubmit">注册</el-button>
+          <div class="df f12 c6 jc_sb mt10">
+            <!-- <div>忘记密码</div> -->
+            <!-- <div class="hand" @click="handleRedister">注册</div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+    <send-code-verify v-if="verifyFlag" @close="closeVarifyDialog" @success="successVerifyDialog"></send-code-verify>
+  </div>
+</template>
+
+<script>
+import sendCodeVerify from '@/components/sendCodeVerify'
+import { register, sendCode, verifyCode } from '@/api'
+import { phoneValidate } from '@/data/fun'
+export default {
+  components: { sendCodeVerify },
+  data() {
+    const validatePhone = (rule, value, callback) => {
+      const str = phoneValidate(value)
+      if (str.length > 0) {
+        callback(new Error(str))
+      } else {
+        callback()
+      }
+    }
+    const validateCode = (rule, value, callback) => {
+      if (this.$fun.trim(this.code) === '') {
+        callback(new Error('请输入验证码'))
+      } else if (this.$fun.trim(this.code).length !== 6) {
+        callback(new Error('验证码格式错误'))
+      } else {
+        callback()
+      }
+    }
+    const validatePassword = (value, rule, callback) => {
+      if (this.formData.password === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        const pwdRegex = new RegExp('(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])')
+        if (!pwdRegex.test(this.formData.password)) {
+          callback(new Error('您的密码复杂度太低（密码中必须包含大小写字母、数字)！'))
+        } else {
+          callback()
+        }
+      }
+    }
+    return {
+      loading: false,
+      formData: {
+        account: '',
+        phone: '',
+        password: ''
+      },
+      msg: '发送验证码',
+      code: '',
+      seconds: 0,
+      rules: {
+        account: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+        password: [
+          { required: true, validator: validatePassword, trigger: 'blur' },
+          { min: 8, max: 14, message: '长度在 8 到 14 个字符', trigger: 'blur' }
+        ],
+        phone: [{ required: true, validator: validatePhone, trigger: 'blur' }],
+        code: [
+          { required: true, validator: validateCode, trigger: 'blur' },
+          { type: 'number', message: '请输入正确的验证码', trigger: 'blur' }
+        ]
+      },
+      verifyFlag: false
+    }
+  },
+  mounted() {
+    // if (this.$cookie.getValue('access_token')) {
+    //   this.$router.push('/index')
+    // }
+  },
+  methods: {
+    handleSubmit() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.loading = true
+          verifyCode({
+            code: this.code,
+            phone: this.formData.phone,
+            type: 1
+          })
+            .then(res => {
+              if (res.code === 200) {
+                this.submitFun()
+              } else {
+                this.$message.warning(res.message)
+                this.loading = false
+              }
+            })
+            .catch(() => {
+              this.$message.warning('注册失败')
+              this.loading = false
+            })
+        }
+      })
+    },
+    submitFun() {
+      this.loading = true
+      register(this.formData)
+        .then(res => {
+          if (res.code === 200) {
+            this.$message.success('注册成功')
+            this.$router.push('/login')
+          } else {
+            this.$message.warning(res.message)
+          }
+          this.loading = false
+        })
+        .catch(() => {
+          this.$message.warning('注册失败')
+          this.loading = false
+        })
+    },
+    handleRedister() {
+      this.$router.push('register')
+    },
+    handleSendCode() {
+      this.$refs.form.validateField('phone', (valid) => {
+        if (valid === '') {
+          if (this.seconds === 0) {
+            this.verifyFlag = true
+          }
+        }
+      })
+    },
+    sendCodeFun(data) {
+      this.loading = true
+      sendCode({
+        phone: this.formData.phone,
+        type: 1,
+        code: data.code,
+        uuid: data.uuid
+      }).then(res => {
+        if (res.code === 200) {
+          this.seconds = 61
+          this.timer()
+        } else {
+          this.$message.error(res.message)
+        }
+        this.loading = false
+      }).catch(() => {
+        this.seconds = 0
+        this.$message.error('验证码获取失败')
+        this.loading = false
+      })
+    },
+    timer() {
+      if (this.seconds > 1) {
+        this.seconds--
+        this.msg = this.seconds + ' 秒'
+        setTimeout(this.timer, 1000)
+      } else {
+        this.msg = '重新发送'
+        this.seconds = 0
+      }
+    },
+    // 图片验证码关闭回调
+    closeVarifyDialog() {
+      this.verifyFlag = false
+    },
+    // 图片验证码提交的回调
+    successVerifyDialog(data) {
+      this.closeVarifyDialog()
+      if (data) {
+        this.sendCodeFun(data)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#register {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-image: url('../../assets/imgs/bg1.png');
+  background-size: 100% auto;
+}
+</style>
